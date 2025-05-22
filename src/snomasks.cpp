923b4241d1ddd66a7e0b5376cd6daaa1b8c714ed
Austin Ellis <siniStar@IRC4Fun.net>
2025-05-22 01:16:00 -0500

    Revert "Update snomasks.cpp"
    This reverts commit dd52c8186c543bc3b12445e39126f84cf0061a1e.

--- src/snomasks.cpp
+++ src/snomasks.cpp
@@ -56,7 +56,6 @@ void SnomaskManager::WriteGlobalSno(char letter, const std::string& text)
 SnomaskManager::SnomaskManager()
 {
 	EnableSnomask('a', "ANNOUNCEMENT");
-	EnableSnomask('b', "Notice");
 	EnableSnomask('c', "CONNECT");
 	EnableSnomask('k', "KILL");
 	EnableSnomask('o', "OPER");
